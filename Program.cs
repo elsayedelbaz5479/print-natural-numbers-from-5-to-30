ef7bf9f94db7b77d6bf8735aafeaa6764e240fb2
elsayedelbaz5479 <121630182+elsayedelbaz5479@users.noreply.github.com>
2022-12-29 20:47:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace طباعت_الاعداد_التامه_من_5الا30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a n1");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a n2");
+            int n2 = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int x = n1; x <= n2; x++)
+            {
+                sum = 0;
+
+                for (int i = 1; i <= x / 2; i++)
+                {
+                    if (x % i == 0)
+                        sum += i;
+                }
+                if (sum == x)
+                    Console.WriteLine(x);
+            }
+        }
+    }
+}
